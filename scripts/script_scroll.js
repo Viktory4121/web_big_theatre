8e8ac691932506c3278c2b4900071af83b7196f6
Viktory4121 <medvedka4121@gmail.com>
2023-06-09 12:31:50 +0300

    add scrolls js

---
+++ scripts/script_scroll.js
@@ -0,0 +1,15 @@
+document.getElementById("button-about-theatre").onclick = function() {
+    document.getElementById("about-theatre").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("button-performance-info").onclick = function() {
+    document.getElementById("performance-info").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("button-partners-info").onclick = function() {
+    document.getElementById("partners-info").scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("button-input-forms").onclick = function() {
+    document.getElementById("input-forms").scrollIntoView({behavior: "smooth"});
+}
