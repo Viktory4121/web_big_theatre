e83f0aa90e2354b6a0c1d429a6bfb84493e82d73
Viktory4121 <medvedka4121@gmail.com>
2023-06-10 23:32:03 +0300

    fix adapt

--- scripts/script_scroll.js
+++ scripts/script_scroll.js
@@ -1,15 +1,30 @@
 document.getElementById("button-about-theatre").onclick = function() {
     document.getElementById("about-theatre").scrollIntoView({behavior: "smooth"});
 }
+document.getElementById("button-about-theatre2").onclick = function() {
+    document.getElementById("about-theatre").scrollIntoView({behavior: "smooth"});
+}
+
 
 document.getElementById("button-performance-info").onclick = function() {
     document.getElementById("performance-info").scrollIntoView({behavior: "smooth"});
 }
+document.getElementById("button-performance-info2").onclick = function() {
+    document.getElementById("performance-info").scrollIntoView({behavior: "smooth"});
+}
+
 
 document.getElementById("button-partners-info").onclick = function() {
     document.getElementById("partners-info").scrollIntoView({behavior: "smooth"});
 }
+document.getElementById("button-partners-info2").onclick = function() {
+    document.getElementById("partners-info").scrollIntoView({behavior: "smooth"});
+}
+
 
 document.getElementById("button-input-forms").onclick = function() {
+    document.getElementById("input-forms").scrollIntoView({behavior: "smooth"});
+}
+document.getElementById("button-input-forms2").onclick = function() {
     document.getElementById("input-forms").scrollIntoView({behavior: "smooth"});
 }
